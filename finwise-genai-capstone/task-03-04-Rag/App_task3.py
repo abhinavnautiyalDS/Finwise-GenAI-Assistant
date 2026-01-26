465618d36ab868ba19416da7d3ec8930bfb468e1
Abhinav Nautiyal <164336356+abhinavnautiyalDS@users.noreply.github.com>
2026-01-26 19:36:15 +0530

    Update App_task3.py

--- finwise-genai-capstone/task-03-04-Rag/App_task3.py
+++ finwise-genai-capstone/task-03-04-Rag/App_task3.py
@@ -1,6 +1,6 @@
 import os
 import streamlit as st
-from langchain.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings, HuggingFaceEndpoint
